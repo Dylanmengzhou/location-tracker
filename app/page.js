2bff2145a9f106e66c088dfeb53915325cdbe111
bimengzhou <dylanmengzhou@gmail.com>
2024-06-27 17:37:41 +0900

    add clean button

--- app/page.js
+++ app/page.js
@@ -1,24 +1,23 @@
 "use client";
-import { useState, useEffect } from 'react';
-import Map from '../components/Map';
+import { useState, useEffect } from "react";
+import Map from "../components/Map";
 export default function Home() {
   const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-      const fetchLocations = async () => {
-          const res = await fetch('/api/location');
-          const data = await res.json();
-          setLocations(data);
-      };
+    const fetchLocations = async () => {
+      const res = await fetch("/api/location");
+      const data = await res.json();
+      setLocations(data);
+    };
 
-      fetchLocations();
+    fetchLocations();
   }, []);
   return (
     <main>
       <div>
-        test
-            <Map locations={locations} />
-        </div>
+        <Map locations={locations} />
+      </div>
     </main>
   );
 }
--- components/Map.js
+++ components/Map.js
@@ -1,52 +1,79 @@
 // components/Map.js
-'use client';
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
-import 'leaflet/dist/leaflet.css';
+"use client";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
 
 const DynamicMapContainer = dynamic(
-    () => import('react-leaflet').then((mod) => mod.MapContainer),
-    { ssr: false }
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
 );
 const DynamicTileLayer = dynamic(
-    () => import('react-leaflet').then((mod) => mod.TileLayer),
-    { ssr: false }
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
 );
 const DynamicMarker = dynamic(
-    () => import('react-leaflet').then((mod) => mod.Marker),
-    { ssr: false }
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
 );
 const DynamicPopup = dynamic(
-    () => import('react-leaflet').then((mod) => mod.Popup),
-    { ssr: false }
+  () => import("react-leaflet").then((mod) => mod.Popup),
+  { ssr: false }
 );
 
 const Map = ({ locations }) => {
-    const [isClient, setIsClient] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+  const [showPins, setShowPins] = useState(true);
 
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    if (!isClient) {
-        return null;
-    }
+  if (!isClient) {
+    return null;
+  }
 
-    return (
-        <DynamicMapContainer center={[0, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
-            <DynamicTileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                maxZoom={19}
-            />
-            {locations.map((location, idx) => (
-                <DynamicMarker key={idx} position={[location.latitude, location.longitude]}>
-                    <DynamicPopup>
-                        Location: ({location.latitude}, {location.longitude}) at {new Date(location.timestamp * 1000).toISOString()}
-                    </DynamicPopup>
-                </DynamicMarker>
-            ))}
-        </DynamicMapContainer>
-    );
+  const togglePins = () => {
+    setShowPins(!showPins);
+  };
+
+  return (
+    <>
+      <button
+        onClick={togglePins}
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          zIndex: 1000,
+        }}
+      >
+        {showPins ? "Hide Pins" : "Show Pins"}
+      </button>
+      <DynamicMapContainer
+        center={[0, 0]}
+        zoom={2}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <DynamicTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maxZoom={19}
+        />
+        {showPins &&
+          locations.map((location, idx) => (
+            <DynamicMarker
+              key={idx}
+              position={[location.latitude, location.longitude]}
+            >
+              <DynamicPopup>
+                Location: ({location.latitude}, {location.longitude}) at{" "}
+                {new Date(location.timestamp * 1000).toISOString()}
+              </DynamicPopup>
+            </DynamicMarker>
+          ))}
+      </DynamicMapContainer>
+    </>
+  );
 };
 
 export default Map;
