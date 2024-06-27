b173129202d8d61a0aefe917ad1ca3a7f86215f8
bimengzhou <dylanmengzhou@gmail.com>
2024-06-27 10:27:05 +0900

    Test2

--- app/page.js
+++ app/page.js
@@ -1,24 +1,24 @@
-// app/page.js
-'use client';
+"use client";
 import { useState, useEffect } from 'react';
 import Map from '../components/Map';
-
 export default function Home() {
-    const [locations, setLocations] = useState([]);
+  const [locations, setLocations] = useState([]);
 
-    useEffect(() => {
-        const fetchLocations = async () => {
-            const res = await fetch('/api/location');
-            const data = await res.json();
-            setLocations(data);
-        };
+  useEffect(() => {
+      const fetchLocations = async () => {
+          const res = await fetch('/api/location');
+          const data = await res.json();
+          setLocations(data);
+      };
 
-        fetchLocations();
-    }, []);
-
-    return (
-        <div>
+      fetchLocations();
+  }, []);
+  return (
+    <main>
+      <div>
+        test
             <Map locations={locations} />
         </div>
-    );
+    </main>
+  );
 }
--- components/Map.js
+++ components/Map.js
@@ -1,24 +1,51 @@
 // components/Map.js
 'use client';
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
 
+const DynamicMapContainer = dynamic(
+    () => import('react-leaflet').then((mod) => mod.MapContainer),
+    { ssr: false }
+);
+const DynamicTileLayer = dynamic(
+    () => import('react-leaflet').then((mod) => mod.TileLayer),
+    { ssr: false }
+);
+const DynamicMarker = dynamic(
+    () => import('react-leaflet').then((mod) => mod.Marker),
+    { ssr: false }
+);
+const DynamicPopup = dynamic(
+    () => import('react-leaflet').then((mod) => mod.Popup),
+    { ssr: false }
+);
+
 const Map = ({ locations }) => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null;
+    }
+
     return (
-        <MapContainer center={[0, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
-            <TileLayer
+        <DynamicMapContainer center={[0, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
+            <DynamicTileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 maxZoom={19}
             />
             {locations.map((location, idx) => (
-                <Marker key={idx} position={[location.latitude, location.longitude]}>
-                    <Popup>
+                <DynamicMarker key={idx} position={[location.latitude, location.longitude]}>
+                    <DynamicPopup>
                         Location: ({location.latitude}, {location.longitude}) at {new Date(location.timestamp * 1000).toISOString()}
-                    </Popup>
-                </Marker>
+                    </DynamicPopup>
+                </DynamicMarker>
             ))}
-        </MapContainer>
+        </DynamicMapContainer>
     );
 };
 
